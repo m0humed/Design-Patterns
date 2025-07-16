15dacf7d4a2685a43af8a05a512d56144313714c
m0humed <qandilmohammed83@gmail.com>
2025-07-17 01:08:36 +0300

    using eager loading with small objects to save resources by deleting unneeded if conditions

---
+++ Sengleton.Eagerloading/LogMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sengleton.Eagerloading
+{
+    public class LogMessage
+    {
+
+        public LogType Type { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public LogMessage(LogType type, string message )
+        {
+            Type = type;
+            Message = message;
+            Timestamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Type}] {Timestamp:yyyy-MM-dd HH:mm:ss}  {Message}";
+        }
+
+    }
+}
---
+++ Sengleton.Eagerloading/MemoryLogger.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sengleton.Eagerloading
+{ 
+    public class MemoryLogger
+    {
+        private int _infoCount;
+        private int _warningCount;
+        private int _errorCount;
+
+        
+        private List<LogMessage> _logMessages = new List<LogMessage>();
+        public ReadOnlyCollection<LogMessage> LogMessages => _logMessages.AsReadOnly();
+        
+       
+
+
+        private void Log(string message , LogType logType )
+        => _logMessages.Add(new LogMessage(logType, message));
+
+        public void LogInfo(string message)
+        {
+            _infoCount++;
+            Log(message, LogType.Info);
+        }
+        public void LogWarning(string message)
+        {
+            _warningCount++;
+            Log(message, LogType.Warning);
+        }
+        public void LogError(string message)
+        {
+            _errorCount++;
+            Log(message, LogType.Error);
+        }
+
+        public void showLogCounts()
+        {
+            foreach (var logMessage in _logMessages)
+            {
+                Console.WriteLine(logMessage);
+            }
+            Console.WriteLine($"Info: {_infoCount}, Warning: {_warningCount}, Error: {_errorCount}");
+        }
+
+        // Apply the Singleton pattern
+        private MemoryLogger()
+        {
+            _infoCount = 0;
+            _warningCount = 0;
+            _errorCount = 0;
+        }
+
+        // Use a static readonly to initialize the intance once
+        private static readonly MemoryLogger _instance = new MemoryLogger();
+
+        public static MemoryLogger Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+
+    }
+}
