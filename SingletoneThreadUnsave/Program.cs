20becd3787ab61f6609e435c6c00a6d3e9e2e438
m0humed <qandilmohammed83@gmail.com>
2025-07-16 23:09:39 +0300

    Add project files.

---
+++ SingletoneThreadUnsave/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SingletoneThreadUnsave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
