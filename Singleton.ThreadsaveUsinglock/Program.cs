9d4aaf3402f3132cd053eb6f5ad83a09c764da7d
m0humed <qandilmohammed83@gmail.com>
2025-07-17 00:41:44 +0300

    Solve multi Thread problem by using lock

---
+++ Singleton.ThreadsaveUsinglock/Program.cs
@@ -0,0 +1,35 @@
+﻿using Singleton.ThreadsaveUsinglock;
+
+namespace Singleton.ThreadsaveUsinglock
+{
+    internal class Program
+    {
+        static MemoryLogger _memoryLogger;
+        static void Main(string[] args)
+        {
+            AssignVoucher("A@A.com", "1234");
+
+            UseVoucher("B@B.com", "5678");
+
+            _memoryLogger.showLogCounts();
+        }
+
+        static void AssignVoucher(string email, string code)
+        {
+            _memoryLogger = MemoryLogger.Instance;
+
+            _memoryLogger.LogInfo($"Voucher assigned to {email} with code {code}");
+
+            _memoryLogger.LogError($"Error assigning voucher to {email} with code {code}");
+        }
+
+        static void UseVoucher(string email, string code)
+        {
+            _memoryLogger = MemoryLogger.Instance;
+
+            _memoryLogger.LogWarning($"Warning using voucher by {email} with code {code}");
+
+            _memoryLogger.LogInfo($"Voucher used by {email} with code {code}");
+        }
+    }
+}
